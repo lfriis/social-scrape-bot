e08584a0dd5a568a9df29e58b95e86f8b2d7a3e1
Larsen Friis <larsenfriis@icloud.com>
2020-06-09 21:00:49 -0400

    Added in functionality to begin writing into google sheets

--- scrape.py
+++ scrape.py
@@ -17,19 +17,30 @@ import requests
 from bs4 import BeautifulSoup
 import os
 import datetime
+import time
 from dotenv import load_dotenv
 from pathlib import Path
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # Pulling sensitive keys from .env file
 load_dotenv()
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
 api_key = os.getenv('API_KEY')
 channel_id = os.getenv('CHANNEL_ID')
 
-date = datetime.datetime.utcnow()
+# Setting up auth for google sheets 
+scope  = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+creds  = ServiceAccountCredentials.from_json_keyfile_name("google_drive_secret.json")
+client = gspread.authorize(creds)
 
+date  = datetime.date.today()
+start = time.time()
+
+google_sheet = client.open('Social Scrape Test').sheet1
+
+# Setting up Social Media URLs
 clinica          = "THE CLINICA"
 clinicaInstagram = 'https://www.instagram.com/theclinica/?__a=1'
 clinicaFacebook  = requests.get('https://www.facebook.com/THECLINICA.CA/')
@@ -50,24 +61,31 @@ def getInstagramData(account, url):
     data     = json.loads(json_url.read())
 
     followerCount  = data['graphql']['user']['edge_followed_by']['count']
-    followingCount = data['graphql']['user']['edge_follow']['count']
     postCount      = data['graphql']['user']['edge_owner_to_timeline_media']['count']
     
-    print('\nINSTAGRAM DATA FOR ' + account)
-    print('Total Following: '      + str(followingCount))
-    print('Total followerCount: '  + str(followerCount))
-    print('Total Posts: '          + str(postCount))
+    instagramData = [followerCount, postCount]
+
+    return instagramData
+
+    # print('\nINSTAGRAM DATA FOR ' + account)
+    # print('Total Following: '      + str(followingCount))
+    # print('Total Followers: '      + str(followerCount))
+    # print('Total Posts: '          + str(postCount))
 
 # Scraping Facebook page
 def getFacebookData(account, url):
     soup  = BeautifulSoup(url.text, 'lxml')
 
-    likeCount    = soup.find("div", text = re.compile('people like this')).text
-    followCount  = soup.find("div", text = re.compile('people follow this')).text
+    likeCount    = soup.find("div", text = re.compile('people like this')).text.split()[0]
+    followCount  = soup.find("div", text = re.compile('people follow this')).text.split()[0]
 
-    print('\nFACEBOOK DATA FOR ' + account)
-    print('Likes: '              + likeCount.split()[0])
-    print('Followers: '          + followCount.split()[0])
+    facebookData = [followCount, likeCount]
+
+    return facebookData
+
+    # print('\nFACEBOOK DATA FOR ' + account)
+    # print('Likes: '              + likeCount)
+    # print('Followers: '          + followCount)
 
 # Pull reporting data from YouTube API
 def getYoutubeData(account, channel_id, api_key):
@@ -81,27 +99,43 @@ def getYoutubeData(account, channel_id, api_key):
     subscriberCount  = data['items'][0]['statistics']['subscriberCount']
     videoCount       = data['items'][0]['statistics']['videoCount']
 
-    print('\nYOUTUBE DATA FOR ' + account)
-    print('Total Views:        ' + viewCount)
-    print('Total Comments:     ' + commentCount)
-    print('Total Subscribers:  ' + subscriberCount)
-    print('Total Videos:       ' + videoCount)
+    youtubeData = [subscriberCount, viewCount, commentCount, videoCount]
 
-def getTikTok(account, url):
-    page = requests.get(url)
-    soup  = BeautifulSoup(url.text, 'lxml')
+    return youtubeData
 
-    data = soup.find('meta', {'name':'description'}).get('content')
+    # print('\nYOUTUBE DATA FOR '  + account)
+    # print('Total Views:        ' + dataArray[0])
+    # print('Total Comments:     ' + dataArray[1])
+    # print('Total Subscribers:  ' + dataArray[2])
+    # print('Total Videos:       ' + dataArray[3])
+
+
+# def getTikTok(account, url):
+#     page = requests.get(url)
+#     soup  = BeautifulSoup(url.text, 'lxml')
+
+#     data = soup.find('meta', {'name':'description'}).get('content')
     
-    print('\nTIKTOK DATA FOR ' + account)
-    print(page)
-    print(soup)
-    print(data)
+#     print('\nTIKTOK DATA FOR ' + account)
+#     print(page)
+#     print(soup)
+#     print(data)
 
-print('\n')
-print('AS OF: ' + str(date))
+# Building list to append to existing spreadsheet
+clinicaRow = [str(date), 
+                getInstagramData (clinica, clinicaInstagram)[0], " ", getInstagramData(clinica, clinicaInstagram)[1],
+                getFacebookData  (clinica, clinicaFacebook)[0],  getFacebookData (clinica, clinicaFacebook)[1],
+                getYoutubeData   (clinica, clinicaChannelID, clinicaAPIKey)[0], getYoutubeData(clinica, clinicaChannelID, clinicaAPIKey)[1],
+                getYoutubeData   (clinica, clinicaChannelID, clinicaAPIKey)[2], getYoutubeData(clinica, clinicaChannelID, clinicaAPIKey)[3]
+             ]
 
-getTikTok(clinica, clinicaTikTok)
+# look into getting youtube comments
+
+# google_sheet.append_row(clinicaRow)
+
+# print("[{}] records inserted on {}...  \n".format(len(clinicaRow), date))
+# print("It took [{}] seconds to execute...\n".format(round(time.time() - start, 2)))
+
 
 # getInstagramData(clinica, clinicaInstagram)
 # getFacebookData (clinica, clinicaFacebook)
